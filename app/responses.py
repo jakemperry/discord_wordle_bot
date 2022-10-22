117bf6aa148d60ef2c1dc99bd34df41f0fbf9334
jakemperry <Jakemperry@gmail.com>
2022-10-21 21:05:50 -0700

    successfully added message intents

--- app/responses.py
+++ app/responses.py
@@ -9,5 +9,6 @@ def handle_response(message) -> str:
 
     if p_message == '!help':
         return "`This is a help message that will be modified to be helpful later.`"
-
+    if p_message == "todays answer" or "today's answer":
+        return "Today's word has five letters, one of which is probably a vowel."
     
